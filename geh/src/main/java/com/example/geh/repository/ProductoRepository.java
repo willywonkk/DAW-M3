fae8394ff64f36b62f3bba3bcd812eab2cab40b5
willywonkk <99023587+willywonkk@users.noreply.github.com>
2025-03-25 19:40:00 +0100

    Add files via upload

---
+++ geh/src/main/java/com/example/geh/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package com.example.geh.repository;
+
+import com.example.geh.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
+
+
+
+
+
+
---
+++ geh/src/test/java/com/example/geh/GehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
