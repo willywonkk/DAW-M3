10aab55722c57b83b1a857b81a341ea213405326
willywonkk <99023587+willywonkk@users.noreply.github.com>
2025-03-25 19:39:38 +0100

    Delete geh directory

--- geh/src/test/java/com/example/geh/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.geh.repository;
-
-import com.example.geh.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
